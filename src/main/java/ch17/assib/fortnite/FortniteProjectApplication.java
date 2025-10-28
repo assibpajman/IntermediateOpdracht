76cd234e20e6e176cc2dfb11907f6aaf94477552
Assib Pajman <assibpajman@Assibs-MacBook-Air.local>
2025-10-28 21:30:47 +0100

    eerste commit

---
+++ src/main/java/ch17/assib/fortnite/FortniteProjectApplication.java
@@ -0,0 +1,13 @@
+package ch17.assib.fortnite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortniteProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortniteProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch17/assib/fortnite/FortniteProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ch17.assib.fortnite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortniteProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
